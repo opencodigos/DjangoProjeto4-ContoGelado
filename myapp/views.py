e4969df04ebbcba0b43f5dc29e3c7da337a34ef9
eticialima <leticialimacgi@gmail.com>
2024-03-19 00:03:18 -0300

    lista de sabores

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import Cobertura, Embalagem, TipoSabor
 
 # Create your views here.
 def inicio(request):
     return render(request, 'inicio.html')
 
 def cardapio(request):
-    return render(request, 'cardapio.html')
+    embalagens = Embalagem.objects.filter(ativo=True)
+    tipo_sabor = TipoSabor.objects.filter(ativo=True)
+    coberturas = Cobertura.objects.filter(ativo=True)
+    context = {
+        'embalagens': embalagens,
+        'tipo_sabor': tipo_sabor,
+        'coberturas': coberturas
+    }
+    return render(request, 'cardapio.html', context)
